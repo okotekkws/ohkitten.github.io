31abdd3e130376ce434814838d25189bf0fd986b
ohkitten <159702336+ohkitten@users.noreply.github.com>
2024-02-11 19:12:11 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 export const robaczkoweShoty = [
     {
-        "url": "https://www.youtube.com/watch?v=OAnZxln5CV8",
+        "url": "https://www.youtube.com/embed/OAnZxln5CV8?si=Ug5cvZsr0Ex97L1M",
         "title": "Afera o Korki",
         "description": "Test 1"
     },
     {
-        "url": "https://www.youtube.com/watch?v=m2y-pGWIQOM",
+        "url": "https://www.youtube.com/embed/m2y-pGWIQOM?si=eNSWMenk1Gb4yH-W",
         "title": "Afera Chomikowa",
         "description": "Test 2"
     }
