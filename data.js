b844df902d6cbf66a025a6c211ab2da50ced8de3
ohkitten <159702336+ohkitten@users.noreply.github.com>
2024-02-11 19:04:03 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+export const robaczkoweShoty = [
+    {
+        "url": "https://www.youtube.com/watch?v=OAnZxln5CV8",
+        "title": "Afera o Korki",
+        "description": "Test 1"
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=m2y-pGWIQOM",
+        "title": "Afera Chomikowa",
+        "description": "Test 2"
+    }
+]
