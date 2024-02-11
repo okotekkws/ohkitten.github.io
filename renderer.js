ad1f056e8482a3ee2a0945027caec0d9d5c3eefe
ohkitten <159702336+ohkitten@users.noreply.github.com>
2024-02-11 19:03:07 +0100

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,27 @@
+import { robaczkoweShoty } from "./data.js";
+
+console.log("renderer");
+
+export function renderVideoCard(video) {
+    const container = document.getElementById('container');
+
+  for (let i = 0; i < robaczkoweShoty.length; i++) {
+    console.log(robaczkoweShoty[i]);
+    container.innerHTML += getElement(robaczkoweShoty[i]);
+  }
+}
+
+export function getVideo(shot) {
+    return `
+    <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+    <div class="bg-white rounded shadow-sm"><iframe width="560" height="315" src="${shot.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <div class="p-4">
+        <h5> <a href="#" class="text-dark">${shot.title}</a></h5>
+        <p class="small text-muted mb-0">${shot.description}</p>
+        <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+}
