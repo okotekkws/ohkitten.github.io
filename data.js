adf30319abdb70c6bb9882fb8f1630c1f13f0b68
ohkitten <159702336+ohkitten@users.noreply.github.com>
2024-02-12 15:36:38 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,22 @@
 export const robaczkoweShoty = [
     {
         "url": "https://www.youtube.com/embed/OAnZxln5CV8?si=Ug5cvZsr0Ex97L1M",
-        "title": "Afera o Korki",
-        "description": "Test 1"
+        "title": "Chłopaki z Raszei - Afera o jedzenie, korki i dostawy, czyli wielka awantura małżeńska - Styczeń '24",
+        "description": ""
     },
     {
         "url": "https://www.youtube.com/embed/m2y-pGWIQOM?si=eNSWMenk1Gb4yH-W",
-        "title": "Afera Chomikowa",
-        "description": "Test 2"
+        "title": "Chłopaki z Raszei - Afera chomikowa, czyli wku*wiony Mario wyłącza 12 chomików - Styczeń 2024",
+        "description": ""
+    },
+    {
+        "url": "https://www.youtube.com/embed/58A7pQM0sUU?si=S39DJ_MdUzSpbord",
+        "title": "Chłopaki z Raszei - Mariusz aresztowany na streamie, idzie do paki - \"15 dni wolności\" - 01.30.2024",
+        "description": ""
+    },
+    {
+        "url": "https://www.youtube.com/embed/HMIk6dD7c1Q?si=6sU69cJJrpnvS_G6",
+        "title": "Chłopaki z Raszei - Wielka Afera Gomułkowa, czyli Bitwa o Telefon - Styczeń 2024",
+        "description": ""
     }
 ]
